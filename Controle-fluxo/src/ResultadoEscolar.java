512741757bb3a0e056b5a33e7cea4d15a83096d3
Italo Ismailey <italoismailey@gmail.com>
2024-11-16 17:05:42 -0300

    Fluxo Condicional

--- Controle-fluxo/src/ResultadoEscolar.java
+++ Controle-fluxo/src/ResultadoEscolar.java
@@ -1,9 +1,12 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 7;
+        int nota = 10;
 
         if(nota >= 7)
             System.out.print("Aprovado!");
+
+        else if (nota >= 5 && nota < 7)
+        System.out.println("Prova Recuperação!");
 
         else   
         System.out.print("Reprovado!");
