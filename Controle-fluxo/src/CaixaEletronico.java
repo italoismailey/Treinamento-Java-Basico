454135f056252b9cb9762a2e46f0e60cbb7933b1
Italo Ismailey <italoismailey@gmail.com>
2024-11-16 17:03:53 -0300

    Atualizacao de Condicionais (if, else, else if)

--- Controle-fluxo/src/CaixaEletronico.java
+++ Controle-fluxo/src/CaixaEletronico.java
@@ -1,10 +1,20 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
-        double saldo =25.0;
-        double valorSolicitado = 26.0;
 
-        if(valorSolicitado < saldo)
+        double saldo =25.0;
+        double valorSolicitado = 10.0;
+
+        if(valorSolicitado < saldo) {
+
             saldo = saldo - valorSolicitado;
-        System.out.print(saldo);
+            System.out.println("Novo valor saldo: " + saldo);
+            
+        }else
+            System.out.println("Saldo insuficiente");
+        
+
+
+
+
     }
 }
---
+++ Controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,14 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 7;
+
+        if(nota >= 7)
+            System.out.print("Aprovado!");
+
+        else   
+        System.out.print("Reprovado!");
+        
+
+    }
+
+}
