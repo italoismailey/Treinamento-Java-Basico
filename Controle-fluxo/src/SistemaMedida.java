958130b2c43b69aee2ac3d57e8143ab3c02f6cd2
Italo Ismailey <italoismailey@gmail.com>
2024-11-16 23:42:49 -0300

    Switch Case

---
+++ Controle-fluxo/src/SistemaMedida.java
@@ -0,0 +1,41 @@
+public class SistemaMedida {
+    
+    public static void main(String[] args) {
+        
+        String sigla = "P";
+
+
+        /*
+        if (sigla == "P")
+            System.out.println("Pequeno");
+        
+            else if (sigla == "M")
+            System.out.println("Médio");
+
+        else if (sigla == "G")
+            System.out.println("Grande");
+        
+            else
+            System.out.println("Indefinido");  */
+
+    /* ESTUDO DE CONDIÇÃO SWICTH CASE */
+        
+        switch (sigla){
+            case "P": {
+                System.out.println("Pequeno");
+                break;
+            }
+            case "M": {
+                System.out.println("Medio");
+                break;
+            }
+            case "G": {
+                System.out.println("Grande");
+                break;
+            }
+            default:
+                System.out.println("Indefinido");
+
+        }
+    }
+}
