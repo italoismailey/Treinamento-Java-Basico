dddae764f1282d5f509fc133c8d8925c235166e1
Italo Ismailey <italoismailey@gmail.com>
2024-11-17 19:50:46 -0300

    Estrutura de repetição Java

---
+++ Estruturas-de-repeticao/Estudo-estruturas-repeticao-Java/src/estruturaRepeticaoForArray.java
@@ -0,0 +1,15 @@
+public class estruturaRepeticaoForArray {
+    public static void main(String[] args) throws Exception {
+
+        String alunos [] = {"JOÃO","JOSE","MARIO","JULIA","MARCOS","MARCIO"};
+                
+        for (int x=0; x < alunos.length; x++) {
+            System.out.println("O aluno no indice x= " + x + " é "+ alunos [x]);
+            
+        }
+
+
+
+        
+    }
+}
---
+++ Estruturas-de-repeticao/Estudo-estruturas-repeticao-Java/src/exemploEstruturaFor.java
@@ -0,0 +1,10 @@
+public class exemploEstruturaFor {
+    public static void main(String[] args) throws Exception {
+        
+        for (int carneirinhos = 1; carneirinhos <= 5; carneirinhos ++){
+            System.out.println("Contando carneirinhos " + carneirinhos);
+            
+        }
+
+    }
+}
